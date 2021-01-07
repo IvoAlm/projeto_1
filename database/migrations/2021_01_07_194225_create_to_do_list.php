242d16fc23b4f887090ad9a6b4adb03aa71d74d1
Ivo Almeida <ivodsalmeida@gmail.com>
2021-01-07 16:43:39 -0300

    edit migration

--- database/migrations/2021_01_07_194225_create_to_do_list.php
+++ database/migrations/2021_01_07_194225_create_to_do_list.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ToDoList extends Migration
+class CreateToDoList extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class ToDoList extends Migration
      */
     public function up()
     {
-        Schema::table('to_do_list', function (Blueprint $table) {
+        Schema::create('to_do_list', function (Blueprint $table) {
             $table->id();
             $table->string('task_name');
             $table->string('description');
@@ -30,8 +30,6 @@ class ToDoList extends Migration
      */
     public function down()
     {
-        Schema::table('to_do_list', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('to_do_list');
     }
 }
